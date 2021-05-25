30715ff0dbe823451bbba9dfd997e01d230407f6
William Barre <william.barre@campusdulac.com>
2021-05-26 00:37:13 +0200

    while cards rouge

--- src/main_rouge.php
+++ src/main_rouge.php
@@ -1,103 +1,52 @@
+<?php
+require 'connect.php';
+?>
 <div style="width: 100%; height: 150px;"></div> <!-- ratraper nav-bar -->
 
-<article class="cards">
-    <div class="del-creat-cont">
-        <img src="./assets/img/logos/icon_croix_bx.svg" alt="icon des suppression"><img src="./assets/img/logos/icon_crayon.svg" alt="icon d'édition">
-    </div>
-    <div class="card-cont">
-        <img src="./assets/img/generic.jpg" alt="photo bouteille">
-        <h2>CHATEAU DE SAINT COME DE PROUTPROUT</h2>
-        <div class="annee">
-            <h4>Année :</h4>
-            <p>2009</p>
-        </div>
-        <div class="cepage">
-            <h4>Cépage(s) :</h4>
-            <p>Grenache / Syrah</p>
-        </div>
-        <div class="pays">
-            <h4>Pays :</h4>
-            <p>France</p>
-        </div>
-        <div class="region">
-            <h4>Région :</h4>
-            <p>Southern Rhone / Gigondas</p>
-        </div>
-        <div class="descri">
-            <h4>Description :</h4>
-        </div>
-        <div class="textarea">
-            <p>Breaking the mold of the classics, this offering will surprise and undoubtedly get tongues wagging with the hints of coffee and tobacco in
-                perfect alignment with more traditional notes. Breaking the mold of the classics, this offering will surprise and
-                undoubtedly get tongues wagging with the hints of coffee and tobacco in
-                perfect alignment with more traditional notes. Sure to please the late-night crowd with the slight jolt of adrenaline it brings.</p>
-        </div>
-    </div>
-</article>
+<?php
+$req = $db->prepare("
+        SELECT *
+        FROM bottle
+        INNER JOIN type
+        ON bottle.id_to_type = type.id_type
+        INNER JOIN year
+        ON bottle.id_year = year.id
+        WHERE type.type = 1
+        ");
+$req->execute();
+var_dump($req->fetchAll())
+?>
 
-<article class="cards">
-    <div class="del-creat-cont">
-        <img src="./assets/img/logos/icon_croix_bx.svg" alt="icon des suppression"><img src="./assets/img/logos/icon_crayon.svg" alt="icon d'édition">
-    </div>
-    <div class="card-cont">
-        <img src="./assets/img/generic.jpg" alt="photo bouteille">
-        <h2>CHATEAU DE SAINT COME</h2>
-        <div class="annee">
-            <h4>Année :</h4>
-            <p>2009</p>
-        </div>
-        <div class="cepage">
-            <h4>Cépage(s) :</h4>
-            <p>Grenache / Syrah</p>
-        </div>
-        <div class="pays">
-            <h4>Pays :</h4>
-            <p>France</p>
-        </div>
-        <div class="region">
-            <h4>Région :</h4>
-            <p>Southern Rhone / Gigondas</p>
-        </div>
-        <div class="descri">
-            <h4>Description :</h4>
-        </div>
-        <div class="textarea">
-            <p>Breaking the mold of the classics, this offering will surprise and undoubtedly get tongues wagging with the hints of coffee and tobacco.</p>
-        </div>
-    </div>
-</article>
-
-<article class="cards">
-    <div class="del-creat-cont">
-        <img src="./assets/img/logos/icon_croix_bx.svg" alt="icon des suppression"><img src="./assets/img/logos/icon_crayon.svg" alt="icon d'édition">
-    </div>
-    <div class="card-cont">
-        <img src="./assets/img/generic.jpg" alt="photo bouteille">
-        <h2>CHATEAU DE SAINT COME</h2>
-        <div class="annee">
-            <h4>Année :</h4>
-            <p>2009</p>
-        </div>
-        <div class="cepage">
-            <h4>Cépage(s) :</h4>
-            <p>Grenache / Syrah</p>
-        </div>
-        <div class="pays">
-            <h4>Pays :</h4>
-            <p>France</p>
-        </div>
-        <div class="region">
-            <h4>Région :</h4>
-            <p>Southern Rhone / Gigondas</p>
-        </div>
-        <div class="descri">
-            <h4>Description :</h4>
-        </div>
-        <div class="textarea">
-            <p>Breaking the mold of the classics, this offering will surprise and undoubtedly get tongues wagging with the hints of coffee and tobacco in
-                perfect alignment with more traditional notes. Breaking the mold of the classics, this offering will surprise and
-                undoubtedly get tongues wagging with the hints of coffee and tobacco in
-                perfect alignment with more traditional notes. Sure to please the late-night crowd with the slight jolt of adrenaline it brings.</p>
-        </div>
-    </div>
-</article>
+<?php while ($cards = $req->fetchObject()) {
+    echo    '<article class="cards">
+            <div class="del-creat-cont">
+                <img src="./assets/img/logos/icon_croix_bx.svg" alt="icon des suppression"><img src="./assets/img/logos/icon_crayon.svg" alt="icon d\'édition">
+            </div>
+            <div class="card-cont">
+                <img src="./assets/img/' . $cards->photo . '" alt="photo de bouteille : ' . $cards->nom . '">
+                <h2>' . $cards->nom . '</h2>
+                <div class="annee">
+                    <h4>Année :</h4>
+                    <p>' . $cards->annee . '</p>
+                </div>
+                <div class="cepage">
+                    <h4>Cépage(s) :</h4>
+                    <p>' . $cards->cepage . '</p>
+                </div>
+                <div class="pays">
+                    <h4>Pays :</h4>
+                    <p>' . $cards->pays . '</p>
+                </div>
+                <div class="region">
+                    <h4>Région :</h4>
+                    <p>' . $cards->region . '</p>
+                </div>
+                <div class="descri">
+                    <h4>Description :</h4>
+                </div>
+                <div class="textarea">
+                    <p>' . $cards->description . '</p>
+                </div>
+            </div>
+        </article>';
+} ?>
